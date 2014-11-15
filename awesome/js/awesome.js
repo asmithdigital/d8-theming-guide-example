82fe1d7ef9bf08a6f9ac53ab2cb7e9e10f28c673
Sander Tirez <sander.tirez@one-agency.be>
2014-11-15 18:16:24 +0100

    Move source from other repo

---
+++ awesome/js/awesome.js
@@ -0,0 +1,8 @@
+(function ($) {
+  "use strict";
+  Drupal.behaviors.mymodule = {
+    attach: function (context, settings) {
+      $('main').append('<p>Hello world</p>');
+    }
+  };
+}(jQuery));
